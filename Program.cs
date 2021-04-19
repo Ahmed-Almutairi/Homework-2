6016a29ff26d40df29be9c9e3677f3d9f3381d01
Ahmed <dev@test.com>
2021-04-19 10:37:47 +0300

    add two functions: HTMLBuilder and CheckOrderMethod

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Homework2
+{
+
+    
+    class Program
+    {
+        static bool CheckOrderMethod(String Source) {
+
+             if (Source.Length%2 !=0|| Source.Length==0)
+            {
+                return false;
+            }
+
+            int i = 0;
+            int j = Source.Length - 1;
+            while (Source[i] == Source[j])
+            {
+                if (i >= j) return true;
+                else
+                {
+                    i++;
+                    j--;
+                }
+            }
+          
+            return false; }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("------------------------CheckOrderMethod---------------------------");
+            Console.WriteLine("Test case 1 (input: <empty>): {0}", CheckOrderMethod(""));
+            Console.WriteLine("Test case 2 (input: 7): {0}", CheckOrderMethod("7"));
+            Console.WriteLine("Test case 3 (input: 101): {0}", CheckOrderMethod("101"));
+            Console.WriteLine("Test case 4 (input: 889988): {0}", CheckOrderMethod("889988"));
+
+            Console.WriteLine("------------------------HTMLBuilder---------------------------");
+
+            HTMLBuilder builder = new HTMLBuilder();
+
+            Console.WriteLine(builder.OpenTag("html lang=\"en\"").OpenTag("head").OpenTag("title").Content("Welcome to HTMLBuilder").EndTag("title").EndTag("head").EndTag("html").getAndClearBuilder());
+        }
+    }
+    class HTMLBuilder
+    {
+        String value;
+
+
+        public HTMLBuilder OpenTag(String tag)
+        {
+
+            this.value += "<" + tag + ">\n";
+
+            return this;
+        }
+
+        public HTMLBuilder Content(String content)
+        {
+            this.value += content + "\n";
+
+            return this;
+        }
+
+        public HTMLBuilder EndTag(String tag)
+        {
+
+            this.value += "</" + tag + ">\n";
+
+            return this;
+        }
+        public String getBuilder()
+        {
+            return this.value;
+        }
+        public String getAndClearBuilder()
+        {
+            String get = this.value;
+            this.value = "";
+            return get;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Homework-2
+# HTMLBuilder and Check Order Method
+
+> HtmlBuilder allows you to writing HTML by C# code.
+
+> Check Order Method allows you to check integer pairs [0-9].
+
+---
+
+## OUTPUT 🖨:
+
+![alt text](outputs.png)
+
+> [▶️To run the program](https://paiza.io/projects/e/PBDSK-PD_yWy3VNvRsNTTw?theme=twilight)
+
+---
+
+##Group information:
+**Instructor:Fai Alotaibi** 👩‍🏫
+
+ **Group Name: The Rea sea Project** 🌊
